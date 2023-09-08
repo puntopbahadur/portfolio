e7724a6a0537609e4e69b1cacb82bac8c3a0eae1
TOP BAHADUR PUN <140620227+puntopbahadur@users.noreply.github.com>
2023-09-08 22:11:55 +0530

    Add files via upload
    new file added

---
+++ script.js
@@ -0,0 +1,74 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+};
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-150px";
+};
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+};
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-300px";
+};
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-400px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw7SGK2cgH6-te891_u_oKFpL8-eYY291RJHVOcNqKrXhRWZ9wLlJqw4lJ2SBbp6Gk/exec'
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message send successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+var typingEffect = new Typed(".multiText", {
+    strings: ["Developer", "Programmer", "Tester", "Blogger"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+})
+
