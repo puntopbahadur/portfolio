ac4de08ae50110e7f3e93e8e2fb362c8554ddf87
TOP BAHADUR PUN <140620227+puntopbahadur@users.noreply.github.com>
2024-10-02 20:56:53 +0530

    Update script.js
    update js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// ------------Typing Effect--------------------
+var typingEffect = new Typed(".multiText", {
+    strings: ["AI Enthusiast", "Data Analyst", "UI Developer", "QA Engineer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+});
+
+// ----------------About tab-------------------------
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname) {
@@ -11,42 +22,7 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 };
 
-
-var sidemenu = document.getElementById("sidemenu");
-function openmenu() {
-    sidemenu.style.right = "0";
-}
-function closemenu() {
-    sidemenu.style.right = "-150px";
-};
-
-
-var sidemenu = document.getElementById("sidemenu");
-function openmenu() {
-    sidemenu.style.right = "0";
-}
-function closemenu() {
-    sidemenu.style.right = "-200px";
-};
-
-
-var sidemenu = document.getElementById("sidemenu");
-function openmenu() {
-    sidemenu.style.right = "0";
-}
-function closemenu() {
-    sidemenu.style.right = "-300px";
-};
-
-var sidemenu = document.getElementById("sidemenu");
-function openmenu() {
-    sidemenu.style.right = "0";
-}
-function closemenu() {
-    sidemenu.style.right = "-400px";
-}
-
-
+// ---------------------Submit form----------------------------
 const scriptURL = 'https://script.google.com/macros/s/AKfycbw7SGK2cgH6-te891_u_oKFpL8-eYY291RJHVOcNqKrXhRWZ9wLlJqw4lJ2SBbp6Gk/exec'
 const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg")
@@ -55,20 +31,14 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
-            msg.innerHTML = "Message send successfully"
+            msg.innerHTML = "Your message sent successfully."
             setTimeout(function () {
                 msg.innerHTML = ""
             }, 5000)
             form.reset()
         })
         .catch(error => console.error('Error!', error.message))
-})
+});
 
-var typingEffect = new Typed(".multiText", {
-    strings: ["Developer", "Programmer", "Tester", "Blogger"],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1500
-})
+
 
